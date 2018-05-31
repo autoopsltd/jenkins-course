db223b9879129b57e1cf995cd9ff47d688254846
AutoOpsLtd <36133994+autoopsltd@users.noreply.github.com>
2018-05-31 18:19:31 +0100

    Personalised for testing

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/autoopsltd/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('autoopsltd@autoopsltd.com')
         }
     }
     triggers {
